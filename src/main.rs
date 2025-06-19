4380ec90c707f885950f6f63a7ec3a3f9529d881
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-19 18:47:32 +0545

    added the basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-mod routes;
 use actix_files::Files;
 use actix_web::App;
 use actix_web::HttpServer;
-use routes::home::home;
+use actix_web::{HttpResponse, Responder, get};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "home.html")]
+pub struct Home {
+    // rustacean: String,
+}
+
+#[get("/")]
+pub async fn home() -> impl Responder {
+    let template = Home {
+        // rustacean: "Santosh".to_string(),
+    };
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template.render().unwrap())
+}
+
+#[derive(Template)]
+#[template(path = "projects.html")]
+pub struct Projects;
+
+#[get("/projects")]
+pub async fn projects() -> impl Responder {
+    let template = Projects;
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template.render().unwrap())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(home)
+            .service(projects)
             .service(Files::new("/static", "./static").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
