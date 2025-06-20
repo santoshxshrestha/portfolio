1b2187f0b7634a5ce24ae2f682e48c039084aa39
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-20 21:25:46 +0545

    The variable is not taken here

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-#![allow(unused)]
 use actix_files::Files;
 use actix_web::App;
 use actix_web::HttpServer;
 use actix_web::{HttpResponse, Responder, get};
 use askama::Template;
 use dotenv::dotenv;
-use reqwest::{self, Client};
+use reqwest;
 use serde::Deserialize;
-use std::clone;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
@@ -92,7 +90,7 @@ pub async fn projects() -> Result<impl Responder, actix_web::Error> {
                 .projects
                 .iter()
                 .find(|project| repo.name == project.name)
-                .map(|project| (repo))
+                .map(|_| (repo))
         })
         .collect();
 
