72514d2607cfb07f6d5796d2db02316c4f8819ac
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-30 23:50:17 +0545

    no migration

--- migrations/20250630174718_create_messages_table.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-portfolio=# CREATE TABLE blog (
-id SERIAL PRIMARY KEY not null,
-title TEXT not null,
-views int not null default 0,
-content TEXT not null, 
-excerpt TEXT not null,
-created_at TIMESTAMP DEFAULT (current_timestamp at time zone 'utc' at time zone 'Asia/Kathmandu') not null
-);
