ca05c0835883f4ea19380b48521f38cd5c5b2861
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-15 20:11:47 +0545

    added the flake and update git ignore

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake for portfolio";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustc
+        cargo
+        rustfmt
+        # openssl
+        rust-analyzer
+      ];
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
