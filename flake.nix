aba1de3aae775b5c4c137c539eb668a686fc0ee6
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-27 21:21:57 +0545

    added nodejs

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
 {
   description = "Flake for portfolio";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        rustfmt
-        openssl
-        rust-analyzer
-        pkg-config
-      ];
-      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          rustc
+          cargo
+          rustfmt
+          openssl
+          rust-analyzer
+          pkg-config
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
     };
-  };
 }
