bee0dd4e3a776be0cd9ce7ad76e1740df5179fed
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-25 00:07:29 +0545

    database: removed the sqlx and some junk of crud

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-#![allow(unused)]
 use actix_files::Files;
 use actix_web::App;
 use actix_web::HttpServer;
 use actix_web::web;
-use actix_web::web::Form;
-use actix_web::{HttpResponse, Responder, get, post};
+use actix_web::{HttpResponse, Responder, get};
 use askama::Template;
 use dotenv::dotenv;
-use pulldown_cmark::{Options, Parser, html};
 use reqwest;
 use serde::Deserialize;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::types::time::PrimitiveDateTime;
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
@@ -171,7 +166,7 @@ pub async fn get_project_cached(cache: &CacheManager) -> Result<Vec<RepoStats>, 
     );
 
     dotenv().ok();
-    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set.");
+    let token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set.");
 
     let client = reqwest::Client::new();
     let response = client
@@ -339,218 +334,8 @@ pub async fn about() -> impl Responder {
         .body(template.render().unwrap())
 }
 
-#[derive(Template)]
-#[template(path = "blog.html")]
-pub struct Blog {
-    messages: Vec<BlogCard>,
-}
-
-#[derive(Debug)]
-struct BlogCard {
-    id: i32,
-    title: String,
-    excerpt: String,
-    created_at: PrimitiveDateTime,
-    views: i32,
-}
-
-#[get("/blog")]
-async fn blog(pool: web::Data<sqlx::PgPool>) -> actix_web::Result<HttpResponse> {
-    let rows = sqlx::query!(
-        r#"
-        select id,title, excerpt,views, created_at
-        from blog
-        order by id desc
-        "#
-    )
-    .fetch_all(&**pool)
-    .await
-    .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    let content: Vec<BlogCard> = rows
-        .into_iter()
-        .map(|row| BlogCard {
-            id: row.id,
-            title: row.title,
-            excerpt: row.excerpt,
-            created_at: row.created_at,
-            views: row.views,
-        })
-        .collect();
-
-    let template = Blog { messages: content };
-    let body = template
-        .render()
-        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?;
-
-    Ok(HttpResponse::Ok().body(body))
-}
-
-#[derive(Template)]
-#[template(path = "admin.html")]
-struct AdminTemplate {
-    messages: Vec<Message>,
-}
-
-#[derive(Debug)]
-struct Message {
-    id: i32,
-    title: String,
-    content: String,
-    excerpt: String,
-}
-
-#[get("/admin")]
-async fn admin(pool: web::Data<sqlx::PgPool>) -> actix_web::Result<HttpResponse> {
-    let rows = sqlx::query!(
-        r#"
-        select id,title, content, excerpt, created_at 
-        from blog
-        order by id desc
-        "#
-    )
-    .fetch_all(&**pool)
-    .await
-    .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    let content: Vec<Message> = rows
-        .into_iter()
-        .map(|row| Message {
-            id: row.id,
-            title: row.title,
-            content: row.content,
-            excerpt: row.excerpt,
-        })
-        .collect();
-
-    let template = AdminTemplate { messages: content };
-    let body = template
-        .render()
-        .map_err(|e| actix_web::error::ErrorInternalServerError(e.to_string()))?;
-
-    Ok(HttpResponse::Ok().body(body))
-}
-
-#[derive(Deserialize)]
-struct NewMessage {
-    title: String,
-    content: String,
-    excerpt: String,
-}
-
-#[post("/send")]
-async fn send_message(
-    pool: web::Data<sqlx::PgPool>,
-    form: Form<NewMessage>,
-) -> actix_web::Result<HttpResponse> {
-    sqlx::query!(
-        "INSERT INTO blog (title,  content, excerpt) VALUES ($1, $2, $3 )",
-        form.title,
-        form.content,
-        form.excerpt,
-    )
-    .execute(&**pool)
-    .await
-    .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    Ok(HttpResponse::SeeOther()
-        .append_header(("Location", "/admin"))
-        .finish())
-}
-
-#[derive(Deserialize)]
-struct DeleteForm {
-    id: i32,
-}
-
-#[post("/delete")]
-async fn delete_message(
-    pool: web::Data<sqlx::PgPool>,
-    form: Form<DeleteForm>,
-) -> actix_web::Result<HttpResponse> {
-    sqlx::query!("DELETE FROM blog WHERE id = $1", form.id)
-        .execute(&**pool)
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    Ok(HttpResponse::SeeOther()
-        .append_header(("Location", "/admin"))
-        .finish())
-}
-
-#[derive(Template)]
-#[template(path = "blog_detail.html")]
-struct BlogDetailTemplate {
-    title: String,
-    created_at: String,
-    content: String,
-    excerpt: String,
-}
-
-#[get("/blog/{id}")]
-async fn blog_detail(
-    pool: web::Data<sqlx::PgPool>,
-    path: web::Path<i32>,
-) -> actix_web::Result<HttpResponse> {
-    let id = path.into_inner();
-    sqlx::query!("update blog set views = views + 1 where id = $1", id)
-        .execute(&**pool)
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    let row = sqlx::query!(
-        "SELECT title, content, created_at,excerpt FROM blog WHERE id = $1",
-        id
-    )
-    .fetch_one(&**pool)
-    .await
-    .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    let parser = Parser::new_ext(&row.content, Options::all());
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
-
-    let template = BlogDetailTemplate {
-        title: row.title,
-        created_at: row.created_at.to_string(),
-        content: html_output,
-        excerpt: row.excerpt,
-    };
-
-    let body = template.render().unwrap();
-    Ok(HttpResponse::Ok().body(body))
-}
-
-#[derive(Deserialize)]
-struct Views {
-    id: i32,
-}
-
-#[post("/blog/views")]
-async fn views(
-    pool: web::Data<sqlx::PgPool>,
-    form: Form<DeleteForm>,
-) -> actix_web::Result<HttpResponse> {
-    sqlx::query!("update blog set views = views + 1 where id = $1", form.id)
-        .execute(&**pool)
-        .await
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    Ok(HttpResponse::SeeOther()
-        .append_header(("Location", "/blog/{id}"))
-        .finish())
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // dotenv().ok();
-    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    // let pool = PgPoolOptions::new()
-    // .max_connections(10)
-    // .connect(&database_url)
-    // .await
-    // .expect("Failed to create pool.");
-
     let cache_manager = web::Data::new(CacheManager::new());
 
     let cache_for_cleanup = cache_manager.clone();
@@ -563,10 +348,7 @@ async fn main() -> std::io::Result<()> {
             .service(home)
             .service(projects)
             .service(about)
-            // .service(blog)
-            // .service(blog_detail)
             .service(Files::new("/static", "./static").show_files_listing())
-            // .app_data(web::Data::new(pool.clone()))
             .app_data(cache_manager.clone())
     })
     .bind(("0.0.0.0", 8080))?
